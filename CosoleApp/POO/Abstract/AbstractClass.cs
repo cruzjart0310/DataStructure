849666d42ff8adee1ced6b28962847bebd72877a
Arturo Cruz <jartcruz@gmial.com>
2022-01-15 16:58:07 -0600

    Se añade proyecto

---
+++ CosoleApp/POO/Abstract/AbstractClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosoleApp.POO.Abstract
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    abstract class AbstractClass
+    {
+        public int MyProperty { get;}
+
+        public abstract string GetPropiertie();
+    }
+
+
+
+}
---
+++ CosoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+//int x = 124;
+//int a = (int)x;
+
+//int r = a;
+
+//List<double> animals = new List<double> { 2.0, 2.3};
+
+//double res = animals.First(x => x>2.3);
+
+
+
+
+
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Ordenando un arreglo de manera inversa!");
+
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//Console.Write($" Orden inicial:");
+//for (int n=0; n<numbers.Length; n++ )
+//{
+//    Console.Write($"{numbers[n]}");
+//}
+
+//Console.Write($"Oreden inverso:");
+//for (int i= numbers.Length-1; i>=0; i--)
+//{
+//    Console.Write($"{numbers[i]}");
+//}
+
+
+////Reverse
+//Console.WriteLine();
+
+
+
